34438d650d0d807025d6b886b56a50fd80051538
James Bolitho <jim2mullered@hotmail.com>
2013-11-14 22:35:20 +0000

    Update _config.php

--- _config.php
+++ _config.php
@@ -2,5 +2,5 @@
 /*
  * Created on 22-11-2012
  *
- * Created by Frank van der Hammen, Axoft IT BV and modified to make compatible with Silverstripe 3.0 by James Bolitho - Inception Design
+ * Created by Frank van der Hammen, Axoft IT BV and modified to make compatible with Silverstripe 3.1 by James Bolitho - Inception Design
  */
